4be64310eae4e382e7d113f3b08ee271eb9eda7e
LISOTECH INNOVATIONS <163443338+lisotech@users.noreply.github.com>
2026-07-28 00:58:35 +0200

    Delete admin.js

--- admin.js
+++
@@ -1,6 +0,0 @@
-if (user === 'admin' && pass === 'Manvid.p') {
-    sessionStorage.setItem('manvid_admin_auth', 'true');
-    closeModals();
-    renderTracks();
-    document.getElementById('uploadModal').style.display = 'flex';
-}
